86f90df3db2cb857e9f4c25686340f1d2ce32993
Humay <crypto347@gmail.com>
2019-06-21 14:40:32 +0400

    logger 2nd way

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
-    
+
 /**
  * Libraries
  */
 
-import { 
+import {
     createStore,
     applyMiddleware,
     compose
@@ -12,33 +12,46 @@ import {
 import {
     combineReducers
 } from 'redux';
-import { 
-    createLogger
-} from 'redux-logger';
+// import {
+//     logger
+// } from 'redux-logger';
+import {
+        createLogger
+    } from 'redux-logger';
+
+import thunk from 'redux-thunk';
 
 // import rootReducers from "../reducers/rootReducer";
 import exampleReducer from '../reducers/exampleReducer';
 
-// const middlewares = [logger];
+/**
+ * first way to add logger
+ */
+
+// const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(
 //     exampleReducer,
-//     {},
-//     applyMiddleware(logger)
-//     );
+//   storeEnhancers(applyMiddleware(logger, thunk))
+// );
 
-const middleware = [];
+
+/**
+ * second way to add logger
+ */
+
+const middleware = [thunk];
 
 /**
  * actions in a collapsed view
  */
 
-const logger = createLogger({collapsed:true});
+const logger = createLogger({ collapsed: true });
 
 /**
  * do not show in production
  */
 
-if (process.env.ENVIRONMENT !== 'production'){
+if (process.env.ENVIRONMENT !== 'production') {
     middleware.push(logger);
 }
 
