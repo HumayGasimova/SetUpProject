bc30af279f9ea77972633a71dcd141142678b37e
Humay <crypto347@gmail.com>
2019-06-21 13:54:23 +0400

    logger middleware added

--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,33 @@
  */
 
 import { 
-    createStore 
+    createStore,
+    applyMiddleware,
+    compose
 } from "redux";
 
 import {
     combineReducers
 } from 'redux';
+import { 
+    createLogger
+} from 'redux-logger';
 
 // import rootReducers from "../reducers/rootReducer";
 import exampleReducer from '../reducers/exampleReducer';
 
-const store = createStore(exampleReducer);
+// const middlewares = [logger];
+// const store = createStore(
+//     exampleReducer,
+//     {},
+//     applyMiddleware(logger)
+//     );
+
+
+//actions in a collapsed view
+const logger = createLogger({collapsed:true});
+
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
+const store = createStoreWithMiddleware(exampleReducer)
+
 export default store;
