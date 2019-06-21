2ef00f56c56c2bc2739b147c86c1728234f064fc
Humay <crypto347@gmail.com>
2019-06-21 10:28:07 +0400

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  devServer: {
+    contentBase: './build',
+    port: 8084,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
